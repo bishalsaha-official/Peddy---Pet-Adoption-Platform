78b45590ad980aa1789b81a7a36d6d2f15fe568e
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-03-12 00:27:42 +0600

    Implement Footer Section

--- Script/script.js
+++ Script/script.js
@@ -8,15 +8,23 @@ const loadCategory = () => {
 // Display Categories
 const displayCategories = (categories) => {
     const buttonContainer = document.getElementById('button-container');
-    categories.forEach(item => {
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <button class="btn py-10 px-10 w-72">
-            <img src=${item.category_icon} /> <p class="font-bold ml-3">${item.category}</p>
+    categories.forEach((item) => {
+        const buttonDiv = document.createElement('div');
+        buttonDiv.innerHTML = `
+        <button id='btnPetItem' onClick="loadPetCategory(${item.category})" class="btn py-10 px-10 w-72">
+            <img src=${item.category_icon} /> 
+            <p class="font-bold ml-3">${item.category}</p>
         </button>
         `
-        buttonContainer.appendChild(div)
+        buttonContainer.appendChild(buttonDiv)
     })
+}
+
+// Load display Pets by Category
+const loadPetCategory =  (pet) => {
+    fetch(`https://openapi.programming-hero.com/api/peddy/category/${pet}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
 
 // Load All Pets
@@ -30,7 +38,7 @@ const loadPets = () => {
 const displayPets = (pets) => {
     const displayItem = document.getElementById('display-item');
     pets.forEach(item => {
-        console.log(item)
+        // console.log(item)
         const div = document.createElement('div');
         div.classList.add("card", "border", "rounded-lg", "p-3");
         div.innerHTML = `
