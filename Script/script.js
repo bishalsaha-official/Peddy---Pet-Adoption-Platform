ece327fc084e3a5379b8acbd9de405a7124bd8e8
bishalsaha-official <cse.bishalsaha@gmail.com>
2025-03-12 01:54:23 +0600

    Implement Liked Pet Item Functionality

--- Script/script.js
+++ Script/script.js
@@ -27,6 +27,24 @@ const loadPetCategory =  (pet) => {
         .then(data => console.log(data))
 }
 
+// Load Liked Pet Item
+const loadLikedItem = (id) =>{
+    fetch(`https://openapi.programming-hero.com/api/peddy/pet/${id}`)
+        .then(res => res.json())
+        .then(data => displayLikedPet(data.petData))
+}
+
+// Display Liked Item
+const displayLikedPet = (items) => {
+    const likedItem = document.getElementById('liked-item');
+    const div = document.createElement('div');
+    div.classList.add("h-20")
+    div.innerHTML = `
+    <img class="rounded-xl" src=${items.image} />
+    `
+    likedItem.appendChild(div);
+}
+
 // Load All Pets
 const loadPets = () => {
     fetch('https://openapi.programming-hero.com/api/peddy/pets')
@@ -40,7 +58,7 @@ const displayPets = (pets) => {
     pets.forEach(item => {
         // console.log(item)
         const div = document.createElement('div');
-        div.classList.add("card", "border", "rounded-lg", "p-3");
+        div.classList.add("card", "border", "border-[rgba(19,19,19,0.1)]", "rounded-lg", "p-3");
         div.innerHTML = `
         <figure>
             <img src=${item.image} />
@@ -56,7 +74,7 @@ const displayPets = (pets) => {
             </ul>
             <hr>
             <div class="flex justify-between mt-5">
-                <button class="btn text-[#0E7A81]"><i class="fa-regular fa-thumbs-up"></i></button>
+                <button onClick="loadLikedItem(${item.petId})" class="btn text-[#0E7A81]"><i class="fa-regular fa-thumbs-up"></i></button>
                 <button class="btn text-[#0E7A81]">Adopt</button>
                 <button class="btn text-[#0E7A81]">Details</button>
             </div>
